9112cb00334405bbc98902b5e24498c744b5c171
rodmar66 <rodrimartinez66@gmail.com>
2020-09-07 23:22:20 -0300

    modificaciones en gamma. Fijate si coincidis con los cambios y avisame!

--- Degadis/ERF.cs
+++ Degadis/ERF.cs
@@ -8,19 +8,21 @@ namespace Degadis
 {
     class ERF
     {
-        public double erf (double xxx)
+        public double Erf (double xxx)
         {
             double half = 0.5;
+            Gamma gamma = new Gamma();
+            Incgamma incgamma = new Incgamma();
 
             if(xxx < 0)
             {
-                System.Windows.Forms.MessageBox.Show(Properties.Resources.aErf); return;
+                System.Windows.Forms.MessageBox.Show(Properties.Resources.aErf); return 1E30; //return falso para que no de error double
             }
             else if(xxx == 0)
             { 
                 return xxx;
             }
-            else { xxx = gaminc(half, xxx * xxx) / gamma(half)}
+            else { return incgamma.Gaminc(half, xxx * xxx) / gamma.Gama(half); }
         }
     }
 }
